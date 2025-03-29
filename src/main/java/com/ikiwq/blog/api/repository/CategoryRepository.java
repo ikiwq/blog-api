aa1a6c45081db4252fe9ae9713500c22b2f120fb
ikiwq <ikiigaiiii@gmail.com>
2025-03-29 23:32:20 +0100

    Added missing repository annotation

--- src/main/java/com/ikiwq/blog/api/repository/CategoryRepository.java
+++ src/main/java/com/ikiwq/blog/api/repository/CategoryRepository.java
@@ -2,9 +2,11 @@ package com.ikiwq.blog.api.repository;
 
 import com.ikiwq.blog.api.model.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     Optional<Category> findBySlug(String slug);
 }
